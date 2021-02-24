fa59e77fa206e269c995d949581bdad1e2db7fc1
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2021-02-24 20:00:28 +0530

    Display the input

---
+++ Week_1/display_input.java
@@ -0,0 +1,28 @@
+package week1;
+import java.util.*;
+public class Display {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		boolean b;
+		float c;
+		double d;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		a=sc.nextInt();
+		b=sc.nextBoolean();
+		c=sc.nextFloat();
+		d=sc.nextDouble();
+		
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+		sc.close();
+	}
+
+}
