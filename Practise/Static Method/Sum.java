628e4f268981e016834de9eb23269b0ffef42977
Kiran Sekhar C <30956805+KiranSekhar@users.noreply.github.com>
2021-03-08 19:57:00 +0530

    static method

--- Practise/Static Method/Sum.java
+++ Practise/Static Method/Sum.java
@@ -3,10 +3,10 @@ public class Sum {
 	public static void main(String[] args) {
 		
 		Calculate e1=new Calculate();
-		int x=e1.calculate(5);
+		int x=e1.calculate(5); // call to a non-static method
 		System.out.println(x);
 		
-		int y=Calculate.calculate(5,6);
+		int y=Calculate.calculate(5,6); // call to a static method
 		System.out.println(y);
 	}
 	
